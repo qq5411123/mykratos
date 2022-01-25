52ca631432912fc99d85e186d2478adc83523adb
gaoc <273354076@qq.com>
2022-01-25 14:50:26 +0800

    change

---
+++ app/bff/admin/internal/service/user.go
@@ -0,0 +1,14 @@
+package service
+
+import (
+	"context"
+	"github.com/go-kratos/beer-shop/api/bff/admin/v1"
+)
+
+func (s *ShopAdmin) ListUser(ctx context.Context, req *v1.ListUserReq) (*v1.ListUserReply, error) {
+	return nil, nil
+}
+
+func (s *ShopAdmin) GetUser(ctx context.Context, req *v1.GetUserReq) (*v1.GetUserReply, error) {
+	return nil, nil
+}
---
+++ app/bff/interface/cmd/server/wire.go
@@ -0,0 +1,24 @@
+//go:build wireinject
+// +build wireinject
+
+// The build tag makes sure the stub is not built in the final build.
+
+package main
+
+import (
+	"github.com/go-kratos/beer-shop/app/bff/interface/internal/biz"
+	"github.com/go-kratos/beer-shop/app/bff/interface/internal/conf"
+	"github.com/go-kratos/beer-shop/app/bff/interface/internal/data"
+	"github.com/go-kratos/beer-shop/app/bff/interface/internal/server"
+	"github.com/go-kratos/beer-shop/app/bff/interface/internal/service"
+
+	"github.com/go-kratos/kratos/v2"
+	"github.com/go-kratos/kratos/v2/log"
+	"github.com/google/wire"
+	tracesdk "go.opentelemetry.io/otel/sdk/trace"
+)
+
+// initApp init kratos application.
+func initApp(*conf.Server, *conf.Registry, *conf.Data, *conf.Auth, log.Logger, *tracesdk.TracerProvider) (*kratos.App, func(), error) {
+	panic(wire.Build(server.ProviderSet, data.ProviderSet, biz.ProviderSet, service.ProviderSet, newApp))
+}
